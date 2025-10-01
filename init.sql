b0b13b8ede6364726d79948dad6fd455a0e1df83
naldo1989 <58909547+naldo1989@users.noreply.github.com>
2025-10-01 10:47:59 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,20 @@
+DROP TABLE usuarios;
+-- Tabla usuarios
 CREATE TABLE IF NOT EXISTS usuarios (
-  id SERIAL PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL,
-  password VARCHAR(200) NOT NULL
+    id SERIAL PRIMARY KEY,
+    dni VARCHAR(20) UNIQUE NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100) NOT NULL
 );
+
+-- Tabla registros
+CREATE TABLE registros (
+    id SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    escuela INT NOT NULL,
+    mesa INT NOT NULL,
+    cantidad INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FO
